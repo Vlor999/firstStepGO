8b47f094305f5266349bf823944ae2a277da2466
Willem Adnet <willem.adnet@grenoble-inp.org>
2025-04-04 13:08:33 +0200

    Plutot bien en vrai de vrai

--- secondSteps/cmd/main.go
+++ secondSteps/cmd/main.go
@@ -1,21 +1,18 @@
 package main
 
-
 import (
 	"fmt"
-
 	"math/rand"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
-    "github.com/faiface/pixel/pixelgl"
-    "golang.org/x/image/colornames"
-    "golang.org/x/image/font/basicfont"
-    "github.com/faiface/pixel/text"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
+	"golang.org/x/image/colornames"
+	"golang.org/x/image/font/basicfont"
 
 	"snake/try"
 )
-
 
 func get2dRandPoint(minX int, maxX int, minY int, maxY int) [2]int {
     return [2]int{
@@ -23,7 +20,6 @@ func get2dRandPoint(minX int, maxX int, minY int, maxY int) [2]int {
         rand.Intn(maxY-minY) + minY,
     }
 }
-
 
 func update_current(currentDirection [2]int, lastDirection [2]int) [2]int {
     if lastDirection != currentDirection {
@@ -35,7 +31,6 @@ func update_current(currentDirection [2]int, lastDirection [2]int) [2]int {
     }
     return currentDirection
 }
-
 
 func update_randomPoint_and_touching(dequePosition *try.Deque, randomPoint *[2]int, isTouching *int, compteur *int, radius int, maxX int, maxY int) {
     currentTouching := try.HandleSnakeApple(dequePosition, *randomPoint, radius * 2)
@@ -56,116 +51,107 @@ func run() {
         radius int = 10
     )
 
-    var (
-        lastDirection [2]int
-        currentDirection [2]int
-        isWin = false
-        
-        isTouching int = 0
-        compteur int = 1
-    )
+	var (
+		lastDirection [2]int
+		currentDirection [2]int
+		isWin bool = true
+		isTouching int = 0
+		compteur int = 1
+		lastScore int = -1
+	)
 
 	cfg := pixelgl.WindowConfig{
-        Title:  "Go Snake",
-        Bounds: pixel.R(0, 0, float64(maxX), float64(maxY)),
-        VSync:  true,
-    }
-    
-    snakeWindow, err := pixelgl.NewWindow(cfg)
-    if err != nil {
-        panic(err)
-    }
-    
-    pos := snakeWindow.GetPos()
-    x, y := pos.X, pos.Y
-    
-    compteurWidth := 200.0
-    compteurHeight := 100.0
-    margin := 10.0
-    
-    compteur_window, err2 := pixelgl.NewWindow(pixelgl.WindowConfig{
-        Title:  "Compteur",
-        Bounds: pixel.R(0, 0, compteurWidth, compteurHeight),
-        VSync:  true,
-    })
-    if err2 != nil {
-        panic(err2)
-    }
-    
-    compteur_window.SetPos(pixel.V(
-        x - compteurWidth - margin,
-        y,
-    ))
-    
+		Title: "Go Snake",
+		Bounds: pixel.R(0, 0, float64(maxX), float64(maxY)),
+		VSync: true,
+	}
 
-    atlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
-	txt := text.New(pixel.V(20, 70), atlas)
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	atlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
+	txt := text.New(pixel.V(20, float64(maxY)-30), atlas)
 	imd := imdraw.New(nil)
-
 	dequePosition := &try.Deque{}
-    
-	randomPoint := get2dRandPoint(radius, maxX - radius, radius,  maxY - radius)
-    
-	for !snakeWindow.Closed() || isWin {
+	randomPoint := get2dRandPoint(radius, maxX-radius, radius, maxY-radius)
+
+	for !win.Closed() && isWin {
 		lastDirection = currentDirection
-        txt.Clear()
+
         switch {
-            case snakeWindow.Pressed(pixelgl.KeyRight):
+            case win.Pressed(pixelgl.KeyRight):
                 currentDirection = [2]int{1, 0}
-            case snakeWindow.Pressed(pixelgl.KeyLeft):
+            case win.Pressed(pixelgl.KeyLeft):
                 currentDirection = [2]int{-1, 0}
-            case snakeWindow.Pressed(pixelgl.KeyUp):
+            case win.Pressed(pixelgl.KeyUp):
                 currentDirection = [2]int{0, 1}
-            case snakeWindow.Pressed(pixelgl.KeyDown):
+            case win.Pressed(pixelgl.KeyDown):
                 currentDirection = [2]int{0, -1}
         }
 
-        currentDirection = update_current(currentDirection, lastDirection)
-    
-        isGood := dequePosition.Verify(radius)
-        if !isGood{
-            fmt.Println("Collision")
-            break
-        }
+		currentDirection = update_current(currentDirection, lastDirection)
 
-        try.UpdateMap(currentDirection, dequePosition, isTouching == 0)
-        head := dequePosition.GetFront()
-        isInBounds := try.IsInBounds(head, maxX, maxY)
-        if !isInBounds {
+		if !dequePosition.Verify(radius) {
+			fmt.Println("Collision")
             isWin = false
-            break
-        }
+			break
+		}
 
-        update_randomPoint_and_touching(dequePosition, &randomPoint, &isTouching, &compteur, radius, maxX, maxY)
-        
-        imd.Clear()
-        for _, pos := range dequePosition.Data {
-            imd.Color = colornames.Green
-            imd.Push(pixel.V(float64(pos[0]), float64(pos[1])))
-            imd.Circle(float64(radius), 0)
-        }
-        
+		try.UpdateMap(currentDirection, dequePosition, isTouching == 0)
+		head := dequePosition.GetFront()
+		if !try.IsInBounds(head, maxX, maxY) {
+			isWin = false
+			break
+		}
+
+		update_randomPoint_and_touching(dequePosition, &randomPoint, &isTouching, &compteur, radius, maxX, maxY)
+
+		win.Clear(colornames.Black)
+		imd.Clear()
+
+		for _, pos := range dequePosition.Data {
+			imd.Color = colornames.Limegreen
+			imd.Push(pixel.V(float64(pos[0]), float64(pos[1])))
+			imd.Circle(float64(radius), 0)
+		}
+
 		imd.Color = colornames.Red
 		imd.Push(pixel.V(float64(randomPoint[0]), float64(randomPoint[1])))
-        
-		snakeWindow.Clear(colornames.Black)
-		imd.Draw(snakeWindow)
-		snakeWindow.Update()
-        
-        compteur_window.Clear(colornames.Black)
-        fmt.Fprintf(txt, "%d", compteur)
-        txt.Draw(compteur_window, pixel.IM)
-        compteur_window.Update()
+		imd.Circle(float64(radius), 0)
 
-        pos := snakeWindow.GetPos()
-        x, y := pos.X, pos.Y
+		imd.Color = pixel.RGBA{R: 0.2, G: 0.2, B: 0.2, A: 0.7}
+		imd.Push(pixel.V(10, float64(maxY)-10))
+		imd.Push(pixel.V(150, float64(maxY)-40))
+		imd.Rectangle(0)
 
-        compteur_window.SetPos(pixel.V(
-            x - compteurWidth - margin,
-            y,
-        ))
+		imd.Draw(win)
+
+		if compteur != lastScore {
+			txt.Clear()
+			fmt.Fprintf(txt, "Score: %d", compteur)
+			lastScore = compteur
+		}
+		txt.Draw(win, pixel.IM)
+
+		win.Update()
 	}
 
+    if !isWin {
+        for !win.Closed(){
+            if win.Pressed(pixelgl.KeyEscape){
+                win.SetClosed(true)
+            }
+            win.Clear(colornames.Black)
+		    imd.Clear()
+            txt.Clear()
+
+            fmt.Fprintf(txt, "Game Over: %d", compteur)
+            txt.Draw(win, pixel.IM)
+            win.Update()
+        }
+    }
 }
 
 func main() {
